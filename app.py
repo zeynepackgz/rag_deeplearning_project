d051034fa6337f18fa0c67c1cf12827e04f1227e
Zeynep <108180721+zeynepackgz@users.noreply.github.com>
2025-03-28 19:50:01 +0300

    app.py

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ def chat():
         app.logger.error(f"Error: {str(e)}")
         return jsonify({"error": "Internal server error"}), 500
 
-
-#if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=5000, debug=True)
+"""
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)"""
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 10000))
